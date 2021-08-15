7540552f13e4802ce3b27b8905afce24a9a029f6
koolcoders2021@gmail.com <koolcoders2021@gmail.com>
2021-08-15 12:41:44 -0400

    day 8 Extract values from sms

--- src/day8_Homework/ExtractValuesFromSMSMessage.java
+++ src/day8_Homework/ExtractValuesFromSMSMessage.java
@@ -2,6 +2,24 @@ package day8_Homework;
 
 public class ExtractValuesFromSMSMessage {
     public static void main(String[] args) {
+        /*
 
+        Write a program which extracts first name, last name, phone number, message text values from given SMS message
+        Given SMS message_1 is: "Sender: [kool coders] From Number<5712202222>, Message: {Hey, kool coders! You are great now! You are going to be even greater!}"
+        Your program should print:
+        First name: kool
+        Last name: coders
+        Phone number: 5712202222
+        Message: Hey, kool coders! You are great now! You are going to be even greater!
+
+
+        Given SMS message_2 is: "Sender: [Elon Mask] From Number<7032212333>, Message: {Hey, I am going to the Moon! Would you like to join me? Reply YES if you are in.}"
+        Your program should print:
+        First name: Elon
+        Last name: Mask
+        Phone number: 7032212333
+        Message: Hey, I am going to the Moon! Would you like to join me? Reply YES if you are in.
+
+         */
     }
 }
