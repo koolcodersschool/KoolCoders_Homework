9348c161e752b8f7c455f02e1a4e6946a3d51da4
koolcoders2021@gmail.com <koolcoders2021@gmail.com>
2021-08-15 12:46:54 -0400

    day 8 print the word between square brackets

---
+++ src/day8_Homework/PrintTheWordBetweenSquareBrackets.java
@@ -0,0 +1,19 @@
+package day8_Homework;
+
+public class PrintTheWordBetweenSquareBrackets {
+    public static void main(String[] args) {
+        /**
+         
+        Write a program prints the word between square brackets [] in the given sentence.
+        Given sentence_1 is: I am a [java] programmer.
+        Your program should print: java
+
+        Given sentence_2 is: I am a [javascript] programmer.
+        Your program should print: javascript
+
+        Given sentence_2 is: I am a [python] programmer.
+        Your program should print: python
+
+         */
+    }
+}
--- src/day8_Homework/PrintTheWordInBetweenSquareBrackets.java
+++
@@ -1,9 +0,0 @@
-package day8_Homework;
-
-public class PrintTheWordInBetweenSquareBrackets {
-    public static void main(String[] args) {
-        /*
-
-         */
-    }
-}
