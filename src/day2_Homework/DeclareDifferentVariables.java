eb0720df5f7a9b4d2f2586ff24717d2726c51da4
koolcoders2021@gmail.com <koolcoders2021@gmail.com>
2021-08-15 13:43:11 -0400

    day 2 Declare Different Variables

---
+++ src/day2_Homework/DeclareDifferentVariables.java
@@ -0,0 +1,26 @@
+package day2_Homework;
+
+public class DeclareDifferentVariables {
+    public static void main(String[] args) {
+        /**
+
+        Create DeclareByte, DeclareFloat, DeclareInt, DeclareDouble, DeclareChar, DeclareBoolean classes
+
+        In DeclareByte class:
+        declare different byte variables and assign 12, 34, 55, 500 and print them. Observe the results.
+
+        In DeclareFloat class:
+        declare different float variables and assign 12f, 34f, 55f, 500f, 1234599988f and print them. Observe the results.
+
+        In DeclareInt class:
+        declare different int variables and assign 12, 34, 55, 500, 32947924792479, 2.3 and print them. Observe the results.
+
+        In DeclareDouble class:
+        declare different double variables and assign 12.4, 34.5, 55, 500, 32947924792479, 2.3 and print them. Observe the results.
+
+        In DeclareChar class:
+        declare different char variables and assign 'D', 65, 65535 and print them. Observe the results.
+
+         */
+    }
+}
