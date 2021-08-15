4fba69d006b9bc2396213329fbc8cee16644b6b0
koolcoders2021@gmail.com <koolcoders2021@gmail.com>
2021-08-15 13:03:42 -0400

    day 8 who is who

--- src/day8_Homework/PrintTheWordBetweenSquareBrackets.java
+++ src/day8_Homework/PrintTheWordBetweenSquareBrackets.java
@@ -3,7 +3,7 @@ package day8_Homework;
 public class PrintTheWordBetweenSquareBrackets {
     public static void main(String[] args) {
         /**
-         
+
         Write a program prints the word between square brackets [] in the given sentence.
         Given sentence_1 is: I am a [java] programmer.
         Your program should print: java
--- src/day8_Homework/WhoIsWho.java
+++ src/day8_Homework/WhoIsWho.java
@@ -2,6 +2,19 @@ package day8_Homework;
 
 public class WhoIsWho {
     public static void main(String[] args) {
+        /**
 
+        Write a program which prints following based on the beginning of the given String
+
+        For example:
+         given String is "Mr. John Doe" your program should print "Man"
+         given String is "Mrs. Barbara Liskov" your program should print "Woman"
+         given String is "Ms. Ada Lovelace" your program should print "Woman"
+         given String is "Dr. Ghassan Atiyeh" your program should print "Doctor"
+         given String is "Prof. Walter Levin" your program should print "Professor"
+
+
+
+         */
     }
 }
