1a915f3f4d67cdbd889f9b36e6bc6ba2a5ec3c30
koolcoders2021@gmail.com <koolcoders2021@gmail.com>
2021-08-15 13:28:20 -0400

    day 9 FizzBuzz

--- src/day9_Homework/FizzBuzz.java
+++ src/day9_Homework/FizzBuzz.java
@@ -2,6 +2,31 @@ package day9_Homework;
 
 public class FizzBuzz {
     public static void main(String[] args) {
+        /**
+
+        Write a program which finds out the given number is FizzBuzz number or Fizz Number or Buzz Number
+
+        For example your program should check numbers between 1 - 20
+        Within given range above your program should check each number and:
+        if the number divides by 3 and 5 without remainder it is FizzBuzz number
+        if the number divides by 3 without remainder it is Fizz number
+        if the number divides by 5 without remainder it is Buzz number
+
+        Your output should look like this within 1 - 20 range:
+        0 is FizzBuzz number
+        3 is Fizz number
+        5 is Buzz number
+        6 is Fizz number
+        9 is Fizz number
+        10 is Buzz number
+        12 is Fizz number
+        15 is FizzBuzz number
+        18 is Fizz number
+        20 is Buzz number
+
+         */
+
+
 
     }
 }
