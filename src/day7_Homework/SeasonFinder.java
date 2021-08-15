ba00a7f074a09a72b0937cc72636058b74a19ae1
koolcoders2021@gmail.com <koolcoders2021@gmail.com>
2021-08-15 12:22:12 -0400

    day 7 season finder

--- src/day7_Homework/SeasonFinder.java
+++ src/day7_Homework/SeasonFinder.java
@@ -2,6 +2,21 @@ package day7_Homework;
 
 public class SeasonFinder {
     public static void main(String[] args) {
+        /**
 
+        Write a program which tells to the user Season based on month number
+
+        When you run your program it should say "Welcome to my program"
+        In the new line it should say "You will enter month number (from 1 to 12) and we will tell you which season is it"
+        In the new line it should say "Enter month number"
+        Then user enters month number like "4" and presses enter
+        Then in the consol it should say "It is Spring"
+        In the new line it should say "If you want to check another option please run your code again"
+        In the new line it should say "Thank you for using koolCoder's program"
+
+        if user enters month number like "12" and presses enter
+        Then in the consol it should say "It is Winter" and etc.
+
+         */
     }
 }
