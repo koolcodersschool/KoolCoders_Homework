8f0afe453887dcf1555dcd52b5c587426967e80d
koolcoders2021@gmail.com <koolcoders2021@gmail.com>
2021-08-15 13:17:45 -0400

    day 9 how many chars in the given Sring

--- src/day9_Homework/HowManyCharsInGivenString.java
+++ src/day9_Homework/HowManyCharsInGivenString.java
@@ -2,6 +2,15 @@ package day9_Homework;
 
 public class HowManyCharsInGivenString {
     public static void main(String[] args) {
+        /**
 
+        Given a character for example "o"
+        Write a program which finds how many times "o" appears in the given String.
+
+        Let's say given char myChar = 'o';
+        given sentences is: "We are kool coders";
+        Your program should print: "There are 3 [o] in the <We are kool coders>"
+
+         */
     }
 }
